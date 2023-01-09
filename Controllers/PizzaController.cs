b1426a5483a9e60ab3da02d56b0538e624dc625d
Nikola Krstevski <nikolakrstevskids@gmail.com>
2023-01-09 15:22:40 +0100

    primo comit  creato controller e modelo

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,34 @@
+﻿using LaMiaPizzeria.Models;
+using LaMiaPizzeria.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace LaMiaPizzeria.Controllers
+{
+    public class PizzaController : Controller
+    {
+      
+
+        public IActionResult Index()
+        {
+            List<Pizza> listaDeiPost = PizzaData.GetPizzas();
+
+            return View("Index", listaDeiPost);
+        }
+
+        public IActionResult Details(int id)
+        {
+            List<Pizza> listaDeiPost = PizzaData.GetPizzas();
+
+            foreach (Pizza pizza in listaDeiPost)
+            {
+                if (pizza.Id == id)
+                {
+                    return View(pizza);
+                }
+            }
+
+            return NotFound("Il post con l'id cercato non esiste!");
+        }
+    }
+}
---
+++ Utils/PizzaData.cs
@@ -0,0 +1,20 @@
+﻿using LaMiaPizzeria.Models;
+using Microsoft.Extensions.Hosting;
+
+namespace LaMiaPizzeria.Utils
+{
+    public class PizzaData
+    {
+        private static List<Pizza> pizzas= new List<Pizza>();
+        public static List<Pizza> GetPizzas()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Pizza pizzaGenerato = new Pizza(i, "Pizza #" + i, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", "https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-215"+i+".jpg?w=300");
+                pizzas.Add(pizzaGenerato);
+            }
+
+            return pizzas;
+        }
+    }
+}
